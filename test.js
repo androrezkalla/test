cb0f7d934f70d8d28e3ecaccfd83d6de14dbaa41
Andro Rezkalla <78698964+androrezkalla@users.noreply.github.com>
2024-07-21 11:48:04 -0400

    m

--- test.js
+++ test.js
@@ -1,20 +1,13 @@
+//web.config url re-wrtie
+
 <configuration>
   <system.webServer>
     <rewrite>
       <rules>
-        <!-- Rule to route API requests to the backend -->
-        <rule name="ReverseProxyRule" stopProcessing="true">
-          <match url="^api/(.*)" />
-          <conditions>
-            <add input="{REQUEST_FILENAME}" matchType="IsFile" negate="true" />
-            <add input="{REQUEST_FILENAME}" matchType="IsDirectory" negate="true" />
-          </conditions>
-          <action type="Rewrite" url="http://localhost:3000/{R:1}" />
-        </rule>
-        <!-- Rule to handle React routes for client-side routing -->
+        <!-- Rule to handle client-side routing -->
         <rule name="ReactRoutes" stopProcessing="true">
           <match url=".*" />
-          <conditions>
+          <conditions logicalGrouping="MatchAll">
             <add input="{REQUEST_FILENAME}" matchType="IsFile" negate="true" />
             <add input="{REQUEST_FILENAME}" matchType="IsDirectory" negate="true" />
           </conditions>
@@ -24,3 +17,43 @@
     </rewrite>
   </system.webServer>
 </configuration>
+
+// ecosystem.config.js FOR PM2 CONFIG
+
+module.exports = {
+  apps: [
+    {
+      name: 'backend-api',
+      script: './server.js',
+      instances: 'max', // Use all available CPU cores
+      exec_mode: 'cluster', // Enables clustering
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 5001
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 5001
+      }
+    }
+  ]
+};
+
+
+
+//routing 
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+// Middleware to handle favicon requests
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
