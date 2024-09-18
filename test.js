440f5ebf8d0ad5f863cc4bd4728dbd429f1bc8af
Andro Rezkalla <78698964+androrezkalla@users.noreply.github.com>
2024-09-18 09:49:59 -0400

    m

--- test.js
+++ test.js
@@ -1,25 +1,6 @@
-const exportToExcel = () => {
-  // Format the check_in_time for each guest
-  const formattedGuests = checkedInGuests.map(guest => ({
-    ...guest,
-    check_in_time: new Date(guest.check_in_time).toLocaleString('en-US', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      hour12: true,
-    })
-  }));
-
-  // Create worksheet from the formatted checked-in guests data
-  const ws = XLSX.utils.json_to_sheet(formattedGuests);
-  
-  // Create a new workbook and append the worksheet
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, 'Checked-In Guests');
-  
-  // Convert the workbook to a binary Excel file
-  XLSX.writeFile(wb, 'checked_in_guests.xlsx');
-};
+<button
+      onClick={() => setAdminView(!adminView)}
+      className="absolute bottom-0 right-0 opacity-0 w-16 h-16"
+    >
+      Switch View
+    </button>
