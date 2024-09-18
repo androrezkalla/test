699c2794e8f98db31769c1b11e94e177c58f1750
Andro Rezkalla <78698964+androrezkalla@users.noreply.github.com>
2024-09-18 09:59:09 -0400

    m

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
-<button
-      onClick={() => setAdminView(!adminView)}
-      className="absolute bottom-0 right-0 opacity-0 w-16 h-16"
-    >
-      Switch View
-    </button>
+useEffect(() => {
+  const fetchCheckedInGuests = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/get-checked-in-guests');
+      setCheckedInGuests(response.data);
+    } catch (error) {
+      console.error('Error fetching checked-in guests:', error);
+    }
+  };
+
+  if (!adminView) {
+    fetchCheckedInGuests();
+  }
+}, [adminView]);
